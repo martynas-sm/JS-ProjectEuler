4048df951d5e5ff28114f82766b2aba3891a6c5a
martynas-sm <smaizys.martynas@gmail.com>
2021-02-07 18:28:42 +0200

    7th problem

---
+++ 7.js
@@ -0,0 +1,28 @@
+// Sieve of Eratosthenes
+var Sieve = new Array(1000001);
+Sieve.fill(true);
+Sieve[1] = false;
+
+for(i = 2; i <= 1000000; i++)
+{
+    if(!Sieve[i])
+        continue;
+    for(k = 2*i; k <= 1000000; k += i)
+    {
+        Sieve[k] = false;
+    }
+}
+
+var count = 1;
+
+for(i = 2; i <= 1000000; i++)
+{
+    if(!Sieve[i])
+        continue;
+    if(count == 10001) // 10001st
+    {
+        console.log(i);
+        break;
+    }
+    count++;
+}
