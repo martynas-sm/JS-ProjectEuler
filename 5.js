73d674319558aa937a22d135729c6d4470a756d1
martynas-sm <smaizys.martynas@gmail.com>
2021-02-07 18:11:53 +0200

    5th problem

---
+++ 5.js
@@ -0,0 +1,18 @@
+function gcd(a, b)
+{
+    if(b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+function lcm(a, b)
+{
+    return a * b / gcd(a, b);
+}
+
+var Arr = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+var ans = 1;
+Arr.forEach(x => ans = lcm(ans, x));
+
+console.log(ans);
