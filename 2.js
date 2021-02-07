e2c9ebbb8aa07af77fae1f26c4ff2ad91d13d0dc
martynas-sm <smaizys.martynas@gmail.com>
2021-02-07 17:54:38 +0200

    Problem 1 update, 2-4 added

---
+++ 2.js
@@ -0,0 +1,12 @@
+var [a, b] = [1, 1];
+var sum = 0;
+while(b < 4000000)
+{
+    a = b + a;
+    [a, b] = [b, a];
+    if(b % 2 == 0)
+    {
+        sum += b;
+    }
+}
+console.log(sum);
---
+++ 3.js
@@ -0,0 +1,13 @@
+var num = 600851475143;
+var factors = [];
+for(var i = 2; i <= Math.sqrt(num); i++)
+{
+    while(num % i == 0)
+    {
+        factors.push(i);
+        num /= i;
+    }
+}
+if(num > 1)
+factors.push(num);
+factors.forEach(x => console.log(x));
